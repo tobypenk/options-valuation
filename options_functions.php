fc5ce81190e21c46a670b7d4b0b55ae3259c2186
Toby Penk <tobypenk@gmail.com>
2020-12-05 14:37:02 -0500

    Remove whitespace

--- options_functions.php
+++ options_functions.php
@@ -1,9 +1,9 @@
 <?php
-	
+
 	//default type should be 'both'
 
 	function black_scholes($S,$K,$r,$t,$s,$type='call') {
-		
+
 		if ($type == 'call') {
 			return ['call' => black_scholes_call($S,$K,$r,$t,$s)];
 		} else if ($type == 'put') {
@@ -17,35 +17,35 @@
 			trigger_error("parameter 'type' in function 'black_scholes' accepts only the values: 'call', 'put', or 'both'. '" . $type . "' was provided.");
 		}
 	}
-	
+
 	function erf($x) {
-		
+
 		/*$p = 0.47047;
 		$a1 = 0.3480242;
 		$a2 = -0.0958798;
 		$a3 = 0.7478556;
 		$t = 1/(1+$p*$x);
 		$er =  1 - exp(-pow($x,2))*($a1*$t + $a2*pow($t,2) + $a3*pow($t,3));*/
-	
+
 		$p = 0.3275911;
 		$a1 = 0.254829592;
 		$a2 = -0.284496736;
 		$a3 = 1.421413741;
 		$a4 = -1.453152027;
 		$a5 = 1.061405429;
-		
+
 		$t = 1/(1+$p*$x);
-		
-		$er = 1 - 
-			exp(-pow($x,2)) * 
+
+		$er = 1 -
+			exp(-pow($x,2)) *
 			(
-				$a1*$t + 
-				$a2*pow($t,2) + 
+				$a1*$t +
+				$a2*pow($t,2) +
 				$a3*pow($t,3) +
-				$a4*pow($t,4) + 
+				$a4*pow($t,4) +
 				$a5*pow($t,5)
 			);
-			
+
 		return $er;
 	}
 
@@ -56,7 +56,7 @@
 	function is_negative($x) {
 		return $x < 0 ? true : false;
 	}
-	
+
 	function normal_cdf($z) {
 
 		$r = 0.5 + erf(abs($z) / sqrt(2)) / 2;
@@ -94,7 +94,7 @@
 		$d1 = black_scholes_d1($S,$K,$r,$t,$s);
 		$n1 = normal_cdf($d1);
 
-		$v0 = option_value($S,$K,$r,$t,$s,'put');		
+		$v0 = option_value($S,$K,$r,$t,$s,'put');
 
 		return [
 			"value" => $v0,
@@ -110,7 +110,7 @@
 
 		$d1 = black_scholes_d1($S,$K,$r,$t,$s);
 		$d2 = black_scholes_d2($s,$t,$d1);
-		
+
 		if ($type == 'call') {
 			$n1 = normal_cdf($d1);
 			$n2 = normal_cdf($d2);
@@ -125,7 +125,7 @@
 
 		return $v;
 	}
-	
+
 	function option_delta($S,$K,$r,$t,$s,$type='call') {
 
 		$d1 = black_scholes_d1($S,$K,$r,$t,$s);
@@ -173,7 +173,7 @@
 	}
 
 	function option_rho($S,$K,$r,$t,$s,$type='call') {
-		
+
 		$d1 = black_scholes_d1($S,$K,$r,$t,$s);
 		$d2 = black_scholes_d2($s,$t,$d1);
 
@@ -188,16 +188,10 @@
 		return $v/100;
 	}
 
-
-
-
-
-
-
 	function sensitivity_V_wrt_S($S,$K,$r,$t,$s,$type='call',$increment=0.1,$increments_plus_minus=40) {
-		
+
 		$total = [];
-		
+
 		if ($type == 'call') {
 			$call_total = [];
 			for ($i=$increments_plus_minus*-1; $i<$increments_plus_minus; $i++) {
@@ -205,9 +199,9 @@
 				$v = option_value($test_S,$K,$r,$t,$s,'call');
 				array_push($call_total,["S"=>$test_S,"V"=>$v]);
 			}
-			
+
 			$total["call"] = $call_total;
-			
+
 		} else if ($type == 'put') {
 			$put_total = [];
 			for ($i=$increments_plus_minus*-1; $i<$increments_plus_minus; $i++) {
@@ -215,35 +209,35 @@
 				$v = option_value($test_S,$K,$r,$t,$s,'put');
 				array_push($put_total,['S'=>$test_S,'V'=>$v]);
 			}
-			
+
 			$total['put'] = $put_total;
-			
+
 		} else if ($type == 'both') {
 			$total['call'] = sensitivity_V_wrt_S($S,$K,$r,$t,$s,'call',$increment,$increments_plus_minus)['call'];
 			$total['put'] = sensitivity_V_wrt_S($S,$K,$r,$t,$s,'put',$increment,$increments_plus_minus)['put'];
-			
+
 		} else {
 			trigger_error("sensitivity calculation requires specification of 'call' or 'put' for parameter 'type'.");
 		}
-		
+
 		return $total;
 	}
-	
+
 	function sensitivity_V_wrt_vol($S,$K,$r,$t,$s,$type='call',$increment=0.01,$increments_plus_minus=40) {
-		
+
 		$total = [];
-		
+
 		if ($type == 'call') {
-			
+
 			$call_total = [];
 			for ($i=$increments_plus_minus*-1; $i<$increments_plus_minus; $i++) {
 				$test_s = $s + $increment * $i;
 				$v = option_value($S,$K,$r,$t,$test_s,'call');
 				array_push($call_total,['vol'=>$test_s,'V'=>$v]);
 			}
-			
+
 			$total['call'] = $call_total;
-			
+
 		} else if ($type == 'put') {
 			$put_total = [];
 			for ($i=$increments_plus_minus*-1; $i<$increments_plus_minus; $i++) {
@@ -251,68 +245,68 @@
 				$v = option_value($S,$K,$r,$t,$test_s,'put');
 				array_push($put_total,['vol'=>$test_s,'V'=>$v]);
 			}
-			
+
 			$total['put'] = $put_total;
-			
+
 		} else if ($type == 'both') {
 			$total['call'] = sensitivity_V_wrt_vol($S,$K,$r,$t,$s,'call',$increment,$increments_plus_minus)['call'];
 			$total['put'] = sensitivity_V_wrt_vol($S,$K,$r,$t,$s,'put',$increment,$increments_plus_minus)['put'];
-			
+
 		} else {
 			trigger_error("sensitivity calculation requires specification of 'call' or 'put' for parameter 'type'.");
 		}
-		
+
 		return $total;
 	}
-	
+
 	function sensitivity_V_wrt_t($S,$K,$r,$t,$s,$type='call') {
-		
+
 		global $days_in_year;
 		$t_days = round($t * $days_in_year);
-		
+
 		$total = [];
-		
+
 		if ($type == 'call') {
-			
+
 			$call_total = [];
-			
+
 			while ($t_days > 1) {
 				$test_t = ($t_days - 1) / $days_in_year;
 				$v = option_value($S,$K,$r,$test_t,$s,'call');
 				array_push($call_total,['t'=>$test_t*$days_in_year,'V'=>$v]);
 				$t_days -= 1;
 			}
-			
+
 			$total['call'] = $call_total;
-			
+
 		} else if ($type == 'put') {
-			
+
 			$put_total = [];
-			
+
 			while ($t_days > 1) {
 				$test_t = ($t_days - 1) / $days_in_year;
 				$v = option_value($S,$K,$r,$test_t,$s,'put');
 				array_push($put_total,['t'=>$test_t*$days_in_year,'V'=>$v]);
 				$t_days -= 1;
 			}
-			
+
 			$total['put'] = $put_total;
-			
+
 		} else if ($type == 'both') {
 			$total['call'] = sensitivity_V_wrt_t($S,$K,$r,$t,$s,'call')['call'];
 			$total['put'] = sensitivity_V_wrt_t($S,$K,$r,$t,$s,'put')['put'];
-			
+
 		} else {
 			trigger_error("sensitivity calculation requires specification of 'call' or 'put' for parameter 'type'.");
 		}
-		
+
 		return $total;
 	}
 
 	function implied_volatility($S,$K,$V,$r,$t,$type='both',$s_init=1.0,$precision=0.0001,$increment=0.1) {
-		
+
 		$s = $s_init;
-		
+
 		if ($type == 'call') {
 			return ['call' => implied_volatility_call($S,$K,$V,$r,$t,$s,$precision,$increment)];
 		} else if ($type == 'put') {
@@ -327,74 +321,55 @@
 		}
 	}
 
-
-
-
-
-
-
 	// these don't need to be two separate functions
 	function implied_volatility_call($S,$K,$V,$r,$t,$s=1.0,$precision=0.0001,$increment=0.1,$iterations=0) {
-		
+
 		$d1 = black_scholes_d1($S,$K,$r,$t,$s);
 		$d2 = black_scholes_d2($s,$t,$d1);
-		
+
 		$n1 = normal_cdf($d1);
 		$n2 = normal_cdf($d2);
 		$v = $S * $n1 - $K * exp(-$r * $t) * $n2;
-		
+
 		//echo abs($V-$v); echo "\r\n"; echo $iterations; echo "\r\n\r\n";
 
 		if ((abs($V-$v) <= $precision) || $iterations == 10000) {
-			
+
 			return [
 				'call_iv' => $s,
 				'iterations' => $iterations
 			];
 		} else {
 			$s = $s + $increment * ($V/$v - 1);
-				
+
 			return implied_volatility_call($S,$K,$V,$r,$t,$s,$precision,$increment,$iterations+1);
 		}
 	}
-	
+
 	function implied_volatility_put($S,$K,$V,$r,$t,$s=1.0,$precision=0.0001,$increment=0.1,$iterations=0) {
-		
+
 		$d1 = black_scholes_d1($S,$K,$r,$t,$s);
 		$d2 = black_scholes_d2($s,$t,$d1);
-		
+
 		$n1p = normal_cdf(-$d1);
 		$n2p = normal_cdf(-$d2);
 		$v = $K * exp(-$r * $t) * $n2p - $S * $n1p;
-		
+
 		//echo abs($V-$v); echo "\r\n"; echo $iterations; echo "\r\n\r\n";
-		
+
 		if ((abs($V-$v) <= $precision) || $iterations == 10000) {
-			
+
 			return [
 				'put_iv' => $s,
 				'iterations' => $iterations
 			];
-			
+
 		} else {
-			
+
 			$s = $s + $increment * ($V/$v - 1);
-			
+
 			return implied_volatility_put($S,$K,$V,$r,$t,$s,$precision,$increment,$iterations+1);
 		}
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 ?>
